72765526f7f90eb87e6fdbf073016926d10f5a42
Shubhodippal <shubhodippal01@gmail.com>
2023-08-03 22:38:12 +0530

    commit

--- Assignment2/three.c
+++ Assignment2/three.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 int main(){
     int state;
     printf("Press 1 to check if two string is equal or not\nPres 2 to check if first string is smaller than second string\nPress 3 to check if second string is greater\nPress 4 to extract left most n character\nPress 5 to extract right most n character");
@@ -7,9 +8,36 @@ int main(){
     {
     case 1:{
 
+        char str1[20]; 
+        char str2[20]; 
+
+        int value;  
+
+        printf("Enter the first string : ");  
+        scanf("%s",str1);  
+        printf("Enter the second string : ");  
+        scanf("%s",str2);
+           
+        value=strcmp(str1,str2);  
+
+        if(value==0){
+            printf("strings are same");  
+        }  
+        else{
+            printf("strings are not same"); 
+        }  
         break;
     }
     case 2:{
+        char str[99];
+
+        printf("Enter the string");
+        fgets(str, 99, stdin);
+        printf("%s", str);
+        int size = sizeof(str);
+        int l = strlen(str);
+
+        printf("%d  %d",size, l);
 
         break;
     }
@@ -26,7 +54,7 @@ int main(){
         break;
     }
     default:
-        printf("");
+        printf(" ");
         break;
     }
 }
