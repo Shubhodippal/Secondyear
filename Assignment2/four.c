a93e921eba1734cc6f17b21d25bc93cc1f89532d
Shubhodippal <shubhodippal01@gmail.com>
2023-08-05 01:36:21 +0530

    commit

---
+++ Assignment2/four.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<malloc.h>
+struct employee
+{
+    char name[20];
+    int salary;
+};
--- Assignment2/three.c
+++ Assignment2/three.c
@@ -29,32 +29,64 @@ int main(){
         break;
     }
     case 2:{
-        char str[99];
+        char str1[20]; 
+        char str2[20]; 
 
-        printf("Enter the string");
-        fgets(str, 99, stdin);
-        printf("%s", str);
-        int size = sizeof(str);
-        int l = strlen(str);
+        printf("Enter the first string : ");  
+        scanf("%s",str1);  
+        printf("Enter the second string : ");  
+        scanf("%s",str2);
 
-        printf("%d  %d",size, l);
+        if(strlen(str1)<strlen(str2)){
+            printf("First String is smaller than the second one");
+        }else{
+            printf("The first string is not smaller than the second one");
+        }
 
         break;
     }
     case 3:{
         
+        char str1[20]; 
+        char str2[20]; 
+
+        printf("Enter the first string : ");  
+        scanf("%s",str1);  
+        printf("Enter the second string : ");  
+        scanf("%s",str2);
+
+        if(strlen(str2)>strlen(str1)){
+            printf("Second String is greater");
+        }else{
+            printf("The second string is not greater");
+        }
         break;
     }
     case 4:{
+        char str1[20];  
 
+        printf("Enter the first string : ");  
+        scanf("%s",str1);  
+
+        char left = str1[0];
+
+        printf("The left most character is:%c", left);
         break;
     }
     case 5:{
+        
+        char str1[20];  
 
+        printf("Enter the first string : ");  
+        scanf("%s",str1);  
+
+        char right = str1[strlen(str1)-1];
+
+        printf("The right most character is:%c", right);
         break;
     }
     default:
-        printf(" ");
+        printf("Wrong Input");
         break;
     }
 }
